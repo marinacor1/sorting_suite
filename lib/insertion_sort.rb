3c3c786cca1a2e64144c3136fe1cae8c5d18fe3f
marinacor1 <marinaspam1234@gmail.com>
2016-02-11 23:42:54 -0700

    tweaking insert code to match pseudocode more closely

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -7,11 +7,11 @@ class InsertionSort
   def sort(unsorted) #[2, 3, 1]
     binding.pry
       unsorted.each do |element|
+        #while in here look at all elements
         if @sorted.empty?
           @sorted << unsorted.first
           unsorted.delete_at(0)
         else
-          num = 0
           #go through each unsorted element and compare it to each sorted element in ascending order until you find the element that is greater than yours, insert unsorted element into the postion before that correct element
 
           #enumerate through unsorted
@@ -22,16 +22,19 @@ class InsertionSort
           # else compare the ue (AT SAME INDEX) to the next se
           #if none are less than add ue to the end
           #loop thru the ask portion until you've gone
-          while num < @sorted.length do
-            if unsorted[0] < @sorted[num] #0
-              @sorted.insert(unsorted[index], index)
-              unsorted.delete_at(index)
+          index = 0
+          while index < @sorted.length
+            # @sorted.each do |character|
+              if element < character #0
+                @sorted.insert(element, index)
+                unsorted.delete_at(index)
+              end
+              index +=1
             end
-            num +=1
           end
         end
+        @sorted
       end
-      @sorted
   end
 
 end
