1050ae8f4f2138393ccb350c9d088be3deb3fa32
marinacor1 <marinaspam1234@gmail.com>
2016-02-11 23:59:10 -0700

    debugging insert

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -5,36 +5,34 @@ class InsertionSort
   end
 
   def sort(unsorted) #[2, 3, 1]
+    @sorted << unsorted.first
+    unsorted.delete_at(0)
     binding.pry
       unsorted.each do |element|
-        #while in here look at all elements
-        if @sorted.empty?
-          @sorted << unsorted.first
-          unsorted.delete_at(0)
-        else
-          #go through each unsorted element and compare it to each sorted element in ascending order until you find the element that is greater than yours, insert unsorted element into the postion before that correct element
-
-          #enumerate through unsorted
-          #compare unsorted element to each element in sorted
-          #ask: is ue < se?
-          #if so ue goes in front of se
-          #ue is deleted from unsorted
-          # else compare the ue (AT SAME INDEX) to the next se
-          #if none are less than add ue to the end
-          #loop thru the ask portion until you've gone
-          index = 0
+        index = 0
           while index < @sorted.length
-            # @sorted.each do |character|
-              if element < character #0
-                @sorted.insert(element, index)
-                unsorted.delete_at(index)
-              end
-              index +=1
+            if element < @sorted[index]#0
+              @sorted.insert(index, element)
+              unsorted.delete_at(index)
+              #if less than
+            elsif @sorted[index] == @sorted.last
+              @sorted << element
+              unsorted.delete_at(index)
+              #what if element is higher than everything in array? 
             end
+        index +=1
           end
-        end
-        @sorted
       end
+    @sorted
   end
 
 end
+
+#enumerate through unsorted
+#compare unsorted element to each element in sorted
+#ask: is ue < se?
+#if so ue goes in front of se
+#ue is deleted from unsorted
+# else compare the ue (AT SAME INDEX) to the next se
+#if none are less than add ue to the end
+#loop thru the ask portion until you've gone
