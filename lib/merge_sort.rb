25dfaee78c2921c9c0dc1b28132ae75d4c63e3f9
marinacor1 <marinaspam1234@gmail.com>
2016-02-12 09:14:45 -0700

    adding instance test to merge. pseudocode

---
+++ lib/merge_sort.rb
@@ -0,0 +1,22 @@
+class MergeSort
+
+end
+
+
+
+
+
+
+
+#take unsorted array
+#split ua into two (if uneven left will be heavier)
+#take left half and split until groups of all twos (if uneven than one is allowed in addition)
+#take the first pair and compare/ switch accordingly
+#merge the pair
+#take the second pair and compare/ switch accordingly
+#merge the pair
+#if there is one then leave it alone
+#merge loner to group
+#TODO some how get the merged couples to compare with other merged couples (How does this order go?)
+
+#if already sorted, it will just split and then merge
--- test/merge_sort_test.rb
+++ test/merge_sort_test.rb
@@ -5,8 +5,18 @@ require 'minitest/pride'
 require 'pry'
 
 class MergeSortTest < Minitest::Test
+  def test_merge_sort_creates_instance
+    sorter = MergeSort.new
+    sorter.instance_of? MergeSort
+  end
+  def test_merge_sort_returns_same_array_if_already_organized
+    skip
+    sorter = MergeSort.new
+    assert_equal ["a", "b", "c", "d"], sorter.sort(["a", "b", "c", "d"])
+
+  end
   def test_merge_sort_sorts_in_ascending_order
-    skip 
+    skip
     sorter = MergeSort.new
     assert_equal ["a", "b", "c", "d"], sorter.sort(["d", "b", "a", "c"])
   end
