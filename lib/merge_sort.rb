f6f34b512d080ff4aee79d5f7e93e31203d797d0
marinacor1 <marinaspam1234@gmail.com>
2016-02-14 10:19:11 -0700

    adding more todo comments for a big bug in small small group

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -4,6 +4,7 @@ class MergeSort
   end
 
   def sort(unsorted)
+    binding.pry
     slice_into_pairs(unsorted)
     @sorted.each do |group| #[[1,5], [3, 7], [9]]
       #TODO after running the merged group [0,8,6] through the compare_pairs method, the numbers are reorganized to [0,6,8]. However, when coming back to the larger group the next pair is [8,6] and the group is [[2,4], [8,6], [0,[8,6]]]. Theory possibly: the pair [8,6] overrides the work done on the merged group. Is it because the small_group is not known to be a part of the @sorted array?
@@ -13,8 +14,10 @@ class MergeSort
         else
         compare_pairs(pair)
         end
+        #TODO is this being called too early?
         small_group = group[-1].push(group[-2]).flatten #[0, 8, 6]
         #TODO seems to add this group, maybe this part isn't necessary or just do a uniq
+       #whenver compare_pairs is called, should be a merged group (no brackets inside)
         compare_pairs(small_group)
       # group.join.chars #["2", "4", "8", "6", "0"]
       #need to join the last two and compare, switch
@@ -41,7 +44,7 @@ class MergeSort
   end
 
   def compare_pairs(pair)
-    binding.pry
+    # binding.pry
     pair.each_with_index do |element, index|
       next_index = index + 1
       if pair[next_index].nil?
