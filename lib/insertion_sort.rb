4b1787842070a0d419c5410237cf150c13d3a9bc
marinacor1 <marinaspam1234@gmail.com>
2016-02-15 16:21:11 -0700

    removing comments in insertion

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -8,12 +8,8 @@ class InsertionSort
     @sorted << unsorted.first
     unsorted.delete_at(0)
     while unsorted.length > 0 do
-#sometimes it'll come up to here and reset the index
         index = 0
       unsorted.each do |element|
-        #why does c get skipped?
-#sometimes it will stay within the iteration. why does it come out of the iteration? is it because something gets deleted?
-        # binding.pry
           while index <= unsorted.length && unsorted.include?(element) do
           if element < @sorted[index]
             @sorted.insert(index, element)
